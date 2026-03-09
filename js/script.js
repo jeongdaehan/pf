01b38e2d65cd07156475e8ebfdb78f5e1206ffe3
jeongdaehan <jeongdaehan@gmail.com>
2026-03-09 10:27:35 +0900

    0309

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 'use strict';
 
+const body = document.body;
 const headerBox = document.querySelector('header .box');
-
+const switchBtns = document.querySelectorAll('header .switch button');
 const originalY = headerBox.offsetTop;
+
+// 페이지 로드시 저장된 테마 확인
+if (localStorage.getItem('theme') === 'dark') {
+  body.classList.add('dk');
+  switchBtns.forEach((el) => el.classList.toggle('on'));
+}
 
 window.addEventListener('scroll', () => {
   if (scrollY >= originalY) {
@@ -10,4 +17,18 @@ window.addEventListener('scroll', () => {
   } else {
     headerBox.classList.remove('sticky');
   }
+});
+
+switchBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    switchBtns.forEach((el) => el.classList.toggle('on'));
+    body.classList.toggle('dk');
+
+    // 테마 상태 저장
+    if (body.classList.contains('dk')) {
+      localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+    }
+  });
 });
